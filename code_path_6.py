9d4866bc5b1913fd041d0554573ffc4d33e94961
unknown <zauguste52@gmail.com>
2025-05-10 23:29:56 -0400

    reverse linked list unit 6

--- code_path_6.py
+++ code_path_6.py
@@ -124,90 +124,90 @@
 # Define your variables and provide a rationale for why you believe your s
 # olution has the stated time and space complexity.
 
-class Node:
-    ''' Node class '''
-    def __init__(self, value, next=None) -> None:
-        self.value = value
-        self.next = next
-    def print_nodes(self, head) -> int:
-        '''print nodes'''
-        while head:
-            print(head.value)
-            head = head.next
-    def help_reverse(self, head):
-        '''helper function'''
-    #   1. draw your prev pointer, current pointer, and next pointer
-    #   2. start at the head/C
-    #   3. hold the next location before updating it. 
-    #   4. make the next location the previous location <- pivot point
-    #   5. update previlous to current
-    #   6. update current location to next using the held position
-    #   7. repeat steps 2 through 6 
-# hold: c.n : None
-    #   None - 1 - NONE 2 - 1 3 - 2 None
-    #                         P      c
-# prev = p
-# cur = c
-# cur.next = c.n   
-        prev = None
-    #   start at the head
-        while head:
-    # we want to hold the next location because we are going to update it
-    # hold the current location before updating it
-            hold = head.next
-    # update the next location with the previous location
-    # pivot point
-            head.next = prev
-    # store the current location in prev before moving to the next location
-            prev = head
-    #   traverse until none
-            head = hold
-    # update head with the next location
-    # return prev
-        return prev
-    def is_palindrome(self,head) -> bool:
-        if not head:
-            return False
-        # get the middle
-        slow,fast = head,head
-        reversed_middle_head = Node(-1)
-        while slow and fast:
-            fast,slow = fast.next.next,slow.next
-            if fast == None:
-                middle = slow
-                # the middle
-                reversed_middle_head = self.help_reverse(middle)
-        # set pointers
-        left,right = head,reversed_middle_head
-        # go until none
-        while right:
-            print(left.value, end=""+"==")
-            print(right.value)
-            if left.value != right.value: #check for uneqal values
-                return False
-            left,right = left.next,right.next
-        return True
-# Example Usage:
-node4 = Node('a')
-node3 = Node('b',node4)
-node2 = Node('b',node3)
-head = Node('a',node2)
-# node4 = Node('i')
+# class Node:
+#     ''' Node class '''
+#     def __init__(self, value, next=None) -> None:
+#         self.value = value
+#         self.next = next
+#     def print_nodes(self, head) -> int:
+#         '''print nodes'''
+#         while head:
+#             print(head.value)
+#             head = head.next
+#     def help_reverse(self, head):
+#         '''helper function'''
+#     #   1. draw your prev pointer, current pointer, and next pointer
+#     #   2. start at the head/C
+#     #   3. hold the next location before updating it. 
+#     #   4. make the next location the previous location <- pivot point
+#     #   5. update previlous to current
+#     #   6. update current location to next using the held position
+#     #   7. repeat steps 2 through 6 
+# # hold: c.n : None
+#     #   None - 1 - NONE 2 - 1 3 - 2 None
+#     #                         P      c
+# # prev = p
+# # cur = c
+# # cur.next = c.n   
+#         prev = None
+#     #   start at the head
+#         while head:
+#     # we want to hold the next location because we are going to update it
+#     # hold the current location before updating it
+#             hold = head.next
+#     # update the next location with the previous location
+#     # pivot point
+#             head.next = prev
+#     # store the current location in prev before moving to the next location
+#             prev = head
+#     #   traverse until none
+#             head = hold
+#     # update head with the next location
+#     # return prev
+#         return prev
+#     def is_palindrome(self,head) -> bool:
+#         if not head:
+#             return False
+#         # get the middle
+#         slow,fast = head,head
+#         reversed_middle_head = Node(-1)
+#         while slow and fast:
+#             fast,slow = fast.next.next,slow.next
+#             if fast == None:
+#                 middle = slow
+#                 # the middle
+#                 reversed_middle_head = self.help_reverse(middle)
+#         # set pointers
+#         left,right = head,reversed_middle_head
+#         # go until none
+#         while right:
+#             print(left.value, end=""+"==")
+#             print(right.value)
+#             if left.value != right.value: #check for uneqal values
+#                 return False
+#             left,right = left.next,right.next
+#         return True
+# # Example Usage:
+# node4 = Node('a')
 # node3 = Node('b',node4)
 # node2 = Node('b',node3)
 # head = Node('a',node2)
-# while cur not none:
-    # hold equals cur.next # None
-    # change cur.next to prev
-    # change prev to cur
-    # change cur to hold
-# return prev
+# # node4 = Node('i')
+# # node3 = Node('b',node4)
+# # node2 = Node('b',node3)
+# # head = Node('a',node2)
+# # while cur not none:
+#     # hold equals cur.next # None
+#     # change cur.next to prev
+#     # change prev to cur
+#     # change cur to hold
+# # return prev
 
-# None ->     1->   None  2->  1   3-> 2 None
-#                                prev     cur 
+# # None ->     1->   None  2->  1   3-> 2 None
+# #                                prev     cur 
 
-CHECK = head.is_palindrome(head)
-print(CHECK)
+# CHECK = head.is_palindrome(head)
+# print(CHECK)
 # # Input List:
 # # 1 -> 2 -> 1
 # # Input: head = 1
@@ -220,19 +220,49 @@ print(CHECK)
 
 
 # Problem 6: Put it in Reverse
-# Given the head of a singly linked list, reverse the list, and return the reversed list. You must reverse the list in place. Return the head of the reversed list.
+# Given the head of a singly linked list, reverse the list, and return the reversed list. 
+# You must reverse the list in place. Return the head of the reversed list.
 
-# Evaluate the time and space complexity of your solution. Define your variables and provide a rationale for why you believe your solution has the stated time and space complexity.
+# Evaluate the time and space complexity of your solution. 
+# Define your variables and provide a rationale for why you believe your solution has the 
+# stated time and space complexity.
 
-# class Node:
-#    def __init__(self, value, next=None):
-#        self.value = value
-#        self.next = next
+class Node:
+    '''reverse linked list'''
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+    def print_rev(self,head):
+        '''print the reversed nodes'''
+        while head:
+            print(head.value,end="->")
+            head = head.next
 
-# def reverse(head):
-# 	pass
-# Example Usage:
-
+    def reverse(self, head):
+        '''reverse head method'''
+        prev = None
+        #   check if we hit None
+        while head:
+            # hold the next
+            hold_next = head.next
+            # hold the nextchange the next to reference the previous location
+            head.next = prev
+            # change the next
+            prev = head
+            # go to the next
+            head = hold_next
+        return prev
+# Example Us
+# age:
+node3 = Node(3)
+node2 = Node(2,node3)
+head = Node(1,node2)
+node = Node(-1)
+node.print_rev(head)
+print('',None)
+reversed_head = node.reverse(head)
+node.print_rev(reversed_head)
+print('',None)
 # # Input List: 1 -> 2 -> 3 -> 4
 # # Input: head = 1
 # Example Output:
